e80ea17bba2b744edada694d89d956f7b723b3f1
Gabrielzeven <49731558+gbenselum@users.noreply.github.com>
2020-09-16 18:43:36 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,9 @@
 from flask import Flask
 application = Flask(__name__)
 
-
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-
-
-print(local_ip)
-
 @application.route("/")
 def hello():
-   # return "Hello World!"
-    return local_ip
+    return "Hello World!"
 
 if __name__ == "__main__":
     application.run()
